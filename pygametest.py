e8cda53eaa88bc935eefada16dd96f6ebcfd7cda
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2014-03-19 16:16:34 -0400

    working game of life iteration - super slow though

--- pygametest.py
+++ pygametest.py
@@ -39,34 +39,53 @@ image_array1 = pygame.surfarray.pixels3d( image )
 image_array2 = pygame.surfarray.pixels3d( image )
 
 for pixel in numpy.nditer(image_array1, flags=['external_loop'], op_flags=['readwrite']):
-    pixel[...] = [0, random.randint(0,1) * 255, 0]
+    pixel[...] = [0, random.randint(0,1) * 254, 0]
 
 pygame.surfarray.blit_array(screen, image_array1)
-
-it = numpy.nditer(image_array2, flags=['multi_index'])
-while not it.finished:
-    x = it.multi_index[0]
-    y = it.multi_index[1]
-    color_index = it.multi_index[2]
-    if (color_index == 0):
-        # pp((x, y, color_index))
-        total = 0
-        total += image_array1[ x-1 ][ y-1 ][ color_index ]
-        total += image_array1[ x-1 ][ y   ][ color_index ]
-        if (y < grid_size[1]-1):
-            total += image_array1[ x-1 ][ y+1 ][ color_index ]
-        total += image_array1[ x   ][ y-1 ][ color_index ]
-        if (y < grid_size[1]-1):
-            total += image_array1[ x   ][ y+1 ][ color_index ]
-        if (x < grid_size[0]-1):
-            total += image_array1[ x+1 ][ y-1 ][ color_index ]
-            total += image_array1[ x+1 ][ y   ][ color_index ]
-            if (y < grid_size[1]-1):
-                total += image_array1[ x+1 ][ y+1 ][ color_index ]
-    it.iternext()
-
-pygame.surfarray.blit_array(screen, image_array2)
-# screen.blit(image, (0,0))
 pygame.display.flip()
-time.sleep(2)
+
+for i in range(200):
+    it = numpy.nditer(image_array2, flags=['multi_index'], op_flags=['readwrite'])
+    while not it.finished:
+        x = it.multi_index[0]
+        y = it.multi_index[1]
+        color_index = it.multi_index[2]
+        if (color_index == 1):
+            # pp((x, y, color_index))
+            total = 0
+            total += image_array1[ x-1 ][ y-1 ][ color_index ]
+            total += image_array1[ x-1 ][ y   ][ color_index ]
+            if (y < grid_size[1]-1):
+                total += image_array1[ x-1 ][ y+1 ][ color_index ]
+            total += image_array1[ x   ][ y-1 ][ color_index ]
+            if (y < grid_size[1]-1):
+                total += image_array1[ x   ][ y+1 ][ color_index ]
+            if (x < grid_size[0]-1):
+                total += image_array1[ x+1 ][ y-1 ][ color_index ]
+                total += image_array1[ x+1 ][ y   ][ color_index ]
+                if (y < grid_size[1]-1):
+                    total += image_array1[ x+1 ][ y+1 ][ color_index ]
+
+            neighbors = int(total / 254)
+            cell = image_array1[x][y][color_index]
+            # pp("old cell: {}, neighbors: {}, total: {}".format(cell, neighbors, total))
+            new_cell = 0
+            if (cell == 0):
+                if (neighbors == 3):
+                    new_cell = 1
+            else:
+                if (neighbors == 2 or neighbors == 3):
+                    new_cell = 1
+                elif (neighbors < 2 or neighbors > 3):
+                    new_cell = 0
+
+            it[0] = new_cell * 254
+        it.iternext()
+
+    pp("update")
+    pygame.surfarray.blit_array(screen, image_array2)
+    # screen.blit(image, (0,0))
+    pygame.display.flip()
+
+    image_array1, image_array2 = image_array2, image_array1
 
