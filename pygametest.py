de2fe7b439056281eca82535fc35a3326acef34f
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2014-06-25 13:07:09 -0400

    clean up pygametest a little

--- pygametest.py
+++ pygametest.py
@@ -19,8 +19,8 @@ pp = pprint.PrettyPrinter(indent=4, width=110).pprint
 #     except pygame.error, message:
 #         print "Cannot load image:", filename
 #         raise SystemExit, message
-
 #     return pygame.surfarray.array3d(image)
+
 # def saveSurface(pixels, filename):
 #     try:
 #         surf = pygame.surfarray.make_surface(pixels)
@@ -28,29 +28,57 @@ pp = pprint.PrettyPrinter(indent=4, width=110).pprint
 #         (width, height, colours) = pixels.shape
 #         surf = pygame.display.set_mode((width, height))
 #         pygame.surfarray.blit_array(surf, pixels)
-
 #     pygame.image.save(surf, filename)
 
 
-grid_size = [512, 384]
+# grid_size = [512, 384]
 
-# grid_size = [160, 120]
-# image = pygame.image.load("glidergun.bmp") # load an image
+grid_size = [160, 120]
+image = pygame.image.load("glidergun.bmp") # load an image
 
 screen = pygame.display.set_mode( grid_size )
 pygame.init()
 
-image = pygame.Surface(grid_size) # create a blank image
+# image = pygame.Surface(grid_size) # create a blank image
 
 image_array1 = pygame.surfarray.pixels3d( image )
 
-for pixel in numpy.nditer(image_array1, flags=['external_loop'], op_flags=['readwrite']):
-    pixel[...] = [0, 255 if random.random() > 0.85 else 0, 0]
+# for pixel in numpy.nditer(image_array1, flags=['external_loop'], op_flags=['readwrite']):
+#     pixel[...] = [0, 255 if random.random() > 0.85 else 0, 0]
 
 image_array1 = image_array1.astype(int)
 
+# setting the borders to red
+image_array1[0, :] = [255, 0, 0]
+image_array1[grid_size[0]-1, :] = [255, 0, 0]
+image_array1[:, 0] = [255, 0, 0]
+image_array1[:, grid_size[1]-1] = [255, 0, 0]
+
 pygame.surfarray.blit_array(screen, image_array1)
 pygame.display.flip()
+
+time.sleep(5)
+
+def game_of_life(grid):
+    """
+    Calculate the next game of life iteration for the passed in ndarray
+    Source http://www.loria.fr/~rougier/teaching/numpy/numpy.html#the-way-of-numpy
+    """
+
+    # create a new array with the count of the neigbors of the previous iteration
+    N = (grid[0:-2,0:-2,1:2] + grid[0:-2,1:-1,1:2] + grid[0:-2,2:,1:2] +
+         grid[1:-1,0:-2,1:2]                               + grid[1:-1,2:,1:2] +
+         grid[2:  ,0:-2,1:2] + grid[2:  ,1:-1,1:2] + grid[2:  ,2:,1:2])
+
+    # if we have thee neighbors and the current cell is dead, give birth
+    birth = (N==(255*3)) & (grid[1:-1,1:-1,1:2] < 255)
+    # if we have 2 or 3 neighbors and the current cell is alive, stay alive
+    survive = ((N==(255*2)) | (N==(255*3))) & (grid[1:-1,1:-1,1:2] == 255)
+    # zero out every cell
+    grid[...] = 0
+    # set surviving cells and birthed cells
+    grid[1:-1,1:-1,1:2][birth | survive] = 255
+
 
 for i in range(600):
     # Print the array with ascii
@@ -65,19 +93,7 @@ for i in range(600):
     #            print("")
     #    it.iternext()
 
-    # create a new array with the count of the neigbors of the previous iteration
-    N = (image_array1[0:-2,0:-2,1:2] + image_array1[0:-2,1:-1,1:2] + image_array1[0:-2,2:,1:2] +
-         image_array1[1:-1,0:-2,1:2]                               + image_array1[1:-1,2:,1:2] +
-         image_array1[2:  ,0:-2,1:2] + image_array1[2:  ,1:-1,1:2] + image_array1[2:  ,2:,1:2])
-
-    # if we have thee neighbors and the current cell is dead, give birth
-    birth = (N==(255*3)) & (image_array1[1:-1,1:-1,1:2] < 255)
-    # if we have 2 or 3 neighbors and the current cell is alive, stay alive
-    survive = ((N==(255*2)) | (N==(255*3))) & (image_array1[1:-1,1:-1,1:2] == 255)
-    # zero out every cell
-    image_array1[...] = 0
-    # set surviving cells and birthed cells
-    image_array1[1:-1,1:-1,1:2][birth | survive] = 255
+    game_of_life(image_array1)
 
     pygame.surfarray.blit_array(screen, image_array1)
     # screen.blit(image, (0,0))
