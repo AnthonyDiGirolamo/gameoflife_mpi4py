f336bdd7de2bd440255d7a8a2cd161d538d7b0a8
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2014-06-25 12:23:57 -0400

    game of life working with ndarray and slicing

--- pygametest.py
+++ pygametest.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import math
 import numpy
 import pygame
 import time
@@ -9,6 +10,8 @@ random.seed()
 
 import pprint
 pp = pprint.PrettyPrinter(indent=4, width=110).pprint
+
+# import pdb
 
 # def getPixelArray(filename):
 #     try:
@@ -28,64 +31,54 @@ pp = pprint.PrettyPrinter(indent=4, width=110).pprint
 
 #     pygame.image.save(surf, filename)
 
-# image = pygame.image.load("Passion_flower.jpg")
 
-grid_size = [160, 120]
+grid_size = [512, 384]
+
+# grid_size = [160, 120]
+# image = pygame.image.load("glidergun.bmp") # load an image
+
 screen = pygame.display.set_mode( grid_size )
 pygame.init()
 
-image = pygame.Surface(grid_size)
+image = pygame.Surface(grid_size) # create a blank image
+
 image_array1 = pygame.surfarray.pixels3d( image )
-image_array2 = pygame.surfarray.pixels3d( image )
 
 for pixel in numpy.nditer(image_array1, flags=['external_loop'], op_flags=['readwrite']):
-    pixel[...] = [0, 254 if random.random() > 0.75 else 0, 0]
+    pixel[...] = [0, 255 if random.random() > 0.85 else 0, 0]
+
+image_array1 = image_array1.astype(int)
 
 pygame.surfarray.blit_array(screen, image_array1)
 pygame.display.flip()
 
-for i in range(200):
-    it = numpy.nditer(image_array2, flags=['multi_index'], op_flags=['readwrite'])
-    while not it.finished:
-        x = it.multi_index[0]
-        y = it.multi_index[1]
-        color_index = it.multi_index[2]
-        if (color_index == 1):
-            # pp((x, y, color_index))
-            total = 0
-            total += image_array1[ x-1 ][ y-1 ][ color_index ]
-            total += image_array1[ x-1 ][ y   ][ color_index ]
-            if (y < grid_size[1]-1):
-                total += image_array1[ x-1 ][ y+1 ][ color_index ]
-            total += image_array1[ x   ][ y-1 ][ color_index ]
-            if (y < grid_size[1]-1):
-                total += image_array1[ x   ][ y+1 ][ color_index ]
-            if (x < grid_size[0]-1):
-                total += image_array1[ x+1 ][ y-1 ][ color_index ]
-                total += image_array1[ x+1 ][ y   ][ color_index ]
-                if (y < grid_size[1]-1):
-                    total += image_array1[ x+1 ][ y+1 ][ color_index ]
+for i in range(600):
+    # Print the array with ascii
+    # it = numpy.nditer(image_array1, flags=['multi_index'], op_flags=['readwrite'])
+    # while not it.finished:
+    #    x = it.multi_index[0]
+    #    y = it.multi_index[1]
+    #    color_index = it.multi_index[2]
+    #    if (color_index == 1):
+    #        print "#" if image_array1[x][y][color_index] == 255 else ".",
+    #        if x == grid_size[0]-1:
+    #            print("")
+    #    it.iternext()
 
-            neighbors = int(total / 254)
-            cell = image_array1[x][y][color_index]
-            pp("old cell: {}, neighbors: {}, total: {}".format(cell, neighbors, total))
-            new_cell = 0
-            if (cell == 0):
-                if (neighbors == 3):
-                    new_cell = 1
-            else:
-                if (neighbors == 2 or neighbors == 3):
-                    new_cell = 1
-                elif (neighbors < 2 or neighbors > 3):
-                    new_cell = 0
+    # create a new array with the count of the neigbors of the previous iteration
+    N = (image_array1[0:-2,0:-2,1:2] + image_array1[0:-2,1:-1,1:2] + image_array1[0:-2,2:,1:2] +
+         image_array1[1:-1,0:-2,1:2]                               + image_array1[1:-1,2:,1:2] +
+         image_array1[2:  ,0:-2,1:2] + image_array1[2:  ,1:-1,1:2] + image_array1[2:  ,2:,1:2])
 
-            it[0] = new_cell * 254
-        it.iternext()
+    # if we have thee neighbors and the current cell is dead, give birth
+    birth = (N==(255*3)) & (image_array1[1:-1,1:-1,1:2] < 255)
+    # if we have 2 or 3 neighbors and the current cell is alive, stay alive
+    survive = ((N==(255*2)) | (N==(255*3))) & (image_array1[1:-1,1:-1,1:2] == 255)
+    # zero out every cell
+    image_array1[...] = 0
+    # set surviving cells and birthed cells
+    image_array1[1:-1,1:-1,1:2][birth | survive] = 255
 
-    pp("update")
-    pygame.surfarray.blit_array(screen, image_array2)
+    pygame.surfarray.blit_array(screen, image_array1)
     # screen.blit(image, (0,0))
     pygame.display.flip()
-
-    image_array1, image_array2 = image_array2, image_array1
-
